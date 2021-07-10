be3c80798fde8b9de35aa91af421c66b91bf1d0f
Kuhy <kwh1019_@naver.com>
2021-07-11 02:24:16 +0900

    Update moveDate

--- choongshinAttend/static/index.js
+++ choongshinAttend/static/index.js
@@ -1,17 +1,45 @@
+var view_date;
 
 function clock() {
-    var target = document.getElementById("this-sunday");
-    var time = new Date();
-    var sunday = new Date(time.setDate(time.getDate() - time.getDay()));
-    
-    var month = sunday.getMonth();
-    var date = sunday.getDate();
-    var day = sunday.getDay();
-    var week = ['일', '월', '화', '수', '목', '금', '토'];
-    target.innerText = 
-    `${month+1}월 ${date}일 ${week[day]}요일 `
+  var target = document.getElementById("this-sunday");
+  if (target.innerText != "-") {
+    return;
+  }
+  var time = new Date();
+  var sunday = new Date(time.setDate(time.getDate() - time.getDay()));
+  view_date = sunday;
+  var month = sunday.getMonth();
+  var date = sunday.getDate();
+  var day = sunday.getDay();
+  var week = ["일", "월", "화", "수", "목", "금", "토"];
+
+  target.innerText = `${month + 1}월 ${date}일 ${week[day]}요일 `;
 }
-setTimeout(function() {
-    clock();
-  }, 500);
+setTimeout(function () {
+  clock();
+}, 500);
 // target.innerText = clock();
+
+function preclock() {
+  var target = document.getElementById("this-sunday");
+  var time = view_date;
+  var prevDate = new Date(time.setDate(time.getDate() - time.getDay() - 7));
+
+  var month = prevDate.getMonth();
+  var date = prevDate.getDate();
+  var day = prevDate.getDay();
+  var week = ["일", "월", "화", "수", "목", "금", "토"];
+  target.innerText = `${month + 1}월 ${date}일 ${week[day]}요일 `;
+}
+
+function nextclock() {
+  var target = document.getElementById("this-sunday");
+  var time = view_date;
+  var nextDate = new Date(time.setDate(time.getDate() - time.getDay() + 7));
+
+  var month = nextDate.getMonth();
+  var date = nextDate.getDate();
+  var day = nextDate.getDay();
+  var week = ["일", "월", "화", "수", "목", "금", "토"];
+  target.innerText = `${month + 1}월 ${date}일 ${week[day]}요일 `;
+}
